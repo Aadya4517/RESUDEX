14536062cecf4dc8ee4091e6a0e8761e3805a1e0
Aadya4517 <guptaaadya73@gmail.com>
2026-02-27 16:14:45 +0530

    Setup Spring Boot backend with health API

---
+++ backend/RESUDEX/src/main/java/com/resudex/ResudexApplication.java
@@ -0,0 +1,13 @@
+package com.resudex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResudexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResudexApplication.class, args);
+	}
+
+}
---
+++ backend/RESUDEX/src/test/java/com/RESUDEX/RESUDEX/ResudexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RESUDEX.RESUDEX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResudexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
